247eecf36296c495e6ac374c8f1994e10357caf3
jadaungeetanjali <jadaungeetanjali@gmail.com>
2019-07-24 12:25:27 +0530

    view separated

--- week-0/day-3/my_todo_app/todo_app/__init__.py
+++ week-0/day-3/my_todo_app/todo_app/__init__.py
@@ -3,6 +3,8 @@ import os
 from flask import Flask
 from flask import request
 
+from flask import render_template
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
@@ -12,13 +14,6 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
-    def todo_view(todos):
-        the_view = 'List of my todos:' + '<br/>'
-        for todo in todos:
-            the_view += (todo) + '<br/>'
-
-        the_view += '---List Ends Here---' + '<br/>'
-        return the_view
 
     def get_todos_by_name(name):
         if name == 'geetanjali':
@@ -39,18 +34,8 @@ def create_app(test_config=None):
         print('----------')
 
         person_todo_list = get_todos_by_name(name)
-        return todo_view(person_todo_list)
-
-    @app.route('/geetanjali')
-    def geetanjali():
-        my_todos = ['running', 'listen music']
-        return todo_view(my_todos)
-	
-    # a simple page that list my todos
-    @app.route('/shivang')
-    def shivang():
-        my_todos = ['wake up', 'coffee']
-        return todo_view(my_todos)
+        #return todo_view(person_todo_list)
+        return render_template('todo_view.html', todos=person_todo_list)
 
     return app
 
